edf6e13b3d68e44845b0207cbdb2bcd2d07e9626
Justin Mayer <entroP@gmail.com>
2020-05-02 14:23:21 +0200

    Re-factor Invoke tasks

--- tasks.py
+++ tasks.py
@@ -6,50 +6,58 @@ from invoke import task
 
 PKG_NAME = "seo"
 PKG_PATH = Path(f"pelican/plugins/{PKG_NAME}")
+BIN_DIR = "bin" if os.name != "nt" else "Scripts"
 ACTIVE_VENV = os.environ.get("VIRTUAL_ENV", None)
 VENV_HOME = Path(os.environ.get("WORKON_HOME", "~/.local/share/virtualenvs"))
 VENV_PATH = Path(ACTIVE_VENV) if ACTIVE_VENV else (VENV_HOME / PKG_NAME)
 VENV = str(VENV_PATH.expanduser())
+VENV_BIN = Path(VENV) / Path(BIN_DIR)
+TOOLS = ["black", "flake8", "isort", "poetry", "pre-commit", "pytest"]
 
-TOOLS = ["poetry", "pre-commit"]
-POETRY = which("poetry") if which("poetry") else (VENV / Path("bin") / "poetry")
-PRECOMMIT = (
-    which("pre-commit") if which("pre-commit") else (VENV / Path("bin") / "pre-commit")
-)
+
+def find_tool(tool):
+    """Locate a tool and return its path, preferring the local virtual environment"""
+    t = VENV_BIN / tool
+    t = t if t.is_file() else which(tool)
+
+    return t
 
 
 @task
 def tests(c):
     """Run the test suite"""
-    c.run(f"{VENV}/bin/pytest", pty=True)
+    pytest = find_tool("pytest")
+    c.run(f"{pytest}", pty=True)
 
 
 @task
 def black(c, check=False, diff=False):
-    """Run Black auto-formatter, optionally with --check or --diff"""
+    """Run Black auto-formatter, optionally with --check and/or --diff"""
     check_flag, diff_flag = "", ""
     if check:
         check_flag = "--check"
     if diff:
         diff_flag = "--diff"
-    c.run(f"{VENV}/bin/black {check_flag} {diff_flag} {PKG_PATH} tasks.py")
+    black = find_tool("black")
+    c.run(f"{black} {check_flag} {diff_flag} {PKG_PATH} tasks.py")
 
 
 @task
 def isort(c, check=False, diff=False):
+    """Run isort import sorter, optionally with -c and/or --diff"""
     check_flag, diff_flag = "", ""
     if check:
         check_flag = "-c"
     if diff:
         diff_flag = "--diff"
-    c.run(
-        f"{VENV}/bin/isort {check_flag} {diff_flag} --recursive {PKG_PATH}/* tasks.py"
-    )
+    isort = find_tool("isort")
+    c.run(f"{isort} {check_flag} {diff_flag} --recursive {PKG_PATH}/* tasks.py")
 
 
 @task
 def flake8(c):
-    c.run(f"{VENV}/bin/flake8 {PKG_PATH} tasks.py")
+    flake8 = find_tool("flake8")
+    c.run(f"{flake8} {PKG_PATH} tasks.py")
 
 
 @task
@@ -64,18 +72,20 @@ def tools(c):
     """Install tools in the virtual environment if not already on PATH"""
     for tool in TOOLS:
         if not which(tool):
-            c.run(f"{VENV}/bin/pip install {tool}")
+            c.run(f"{VENV_BIN}/pip install {tool}")
 
 
 @task
 def precommit(c):
     """Install pre-commit hooks to .git/hooks/pre-commit"""
-    c.run(f"{PRECOMMIT} install")
+    precommit = find_tool("pre-commit")
+    c.run(f"{precommit} install")
 
 
 @task
 def setup(c):
-    c.run(f"{VENV}/bin/pip install -U pip")
+    c.run(f"{VENV_BIN}/pip install -U pip")
     tools(c)
-    c.run(f"{POETRY} install")
+    poetry = find_tool("poetry")
+    c.run(f"{poetry} install")
     precommit(c)
